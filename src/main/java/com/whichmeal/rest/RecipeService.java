0d588c8206410f924badf954f53d4813db3bedc6
qmp <glang@lavabit.com>
2012-04-01 17:01:11 +0200

    Add EntityManager

--- src/main/java/com/whichmeal/rest/RecipeService.java
+++ src/main/java/com/whichmeal/rest/RecipeService.java
@@ -1,6 +1,9 @@
 
 package com.whichmeal.rest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.PathParam;
@@ -16,13 +19,14 @@ import ch.bfh.whichmeal.Recipe;
 @Path("json/recipe")
 @Produces("Application/json")
 public class RecipeService{
+    @PersistenceContext(unitName="whichmeal")
+    EntityManager em;
     String name;
     
     @GET
-    @Path("{name}")
-    public Recipe byName(@PathParam("name") final String name){
-        Recipe recipe = new Recipe();
-        recipe.setName(name);
+    @Path("by_id/{id}")
+    public Recipe byId(@PathParam("id") final int id) {
+        Recipe recipe = em.find(Recipe.class, id);
         return recipe;
     }
 }
