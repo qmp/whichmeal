3a7a6548f806e1c7a24d8465c5526dcdf6ee8d42
qmp <glang@lavabit.com>
2012-04-01 15:05:44 +0200

    Default context switch to /api for jboss AS-7

---
+++ src/main/java/com/whichmeal/rest/WhichMealApp.java
@@ -0,0 +1,13 @@
+
+package com.whichmeal.rest;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+/**
+ *
+ * @author qmp
+ */
+@ApplicationPath("/api")
+public class WhichMealApp extends Application{
+    
+}
